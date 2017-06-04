3426114b3f913c0c54fbb7411d77b8f12b89740d
Herman Yanush <herman.yanush2@gmail.com>
2017-06-04 17:10:05 +0300

    Improve GUI design

--- view/application.py
+++ view/application.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 
 class Application(tk.Frame):
+    BUTTON_FONT = ("Arial", 16)
+    LABEL_FONT = ("Helvetica", 32)
+
     def __init__(self, tkinter, master_view):
         super().__init__(tkinter)
         self.tkinter = tkinter
@@ -12,20 +15,23 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.lbl_time = tk.Label(text="00:00:00")
-        self.lbl_time.pack(side="top")
+        self.lbl_time = tk.Label(self, text="00:00:00", font=self.LABEL_FONT)
+        self.lbl_time.pack(side="bottom")
 
         self.btn_startpause = tk.Button(self)
         self.btn_startpause["text"] = "Start"
+        self.btn_startpause["font"] = self.BUTTON_FONT
+        self.btn_startpause["width"] = 15
         self.btn_startpause["command"] = self.master_view.handle_startpause
         self.btn_startpause.pack(side="left")
 
         self.btn_stop = tk.Button(self)
         self.btn_stop["text"] = "Stop"
+        self.btn_stop["font"] = self.BUTTON_FONT
         self.btn_stop["command"] = self.master_view.handle_stop
         self.btn_stop.pack(side="left")
         self.btn_stop["state"] = "disabled"
 
-        self.quit = tk.Button(self, text="Quit", fg="red",
+        self.quit = tk.Button(self, text="Quit", fg="red", font=self.BUTTON_FONT,
                               command=self.master_view.handle_quit)
         self.quit.pack(side="right")
